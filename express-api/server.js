c40de35efc87628a74979c392325dbf661ebe3f3
Alegarciy <ale.garciy@gmail.com>
2024-09-06 01:50:01 -0600

    Implement health checks

--- express-api/server.js
+++ express-api/server.js
@@ -3,13 +3,17 @@ const cors = require('cors');
 const postsRouter = require('./routes/posts');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/posts', postsRouter);
 
+app.get('/api/v1/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
